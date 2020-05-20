5014cde5cf2247702822134250230f456dcac20c
weekyeon <weekyeon@gmail.com>
2020-05-20 18:18:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/javabasicpractice_20200520/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.javabasicpractice_20200520;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.javabasicpractice_20200520.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+}
